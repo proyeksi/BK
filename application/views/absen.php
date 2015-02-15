52bcacdeb190839b19118c10aac0da71a18a88af
askmeadi <adi.satria.k@mail.ugm.ac.id>
2015-02-15 15:01:50 +0700

    final

--- application/views/absen.php
+++ application/views/absen.php
@@ -4,7 +4,7 @@
       <!--main content start-->
       <section id="main-content">
         <section class="wrapper">
-          <h3><i class="fa fa-angle-right"></i> Edit Data</h3>
+          <h3><i class="fa fa-angle-right"></i> Absensi</h3>
 
           <?php
           if(isset($breadcrumb)&& is_array($breadcrumb) && count($breadcrumb) > 0){
@@ -25,11 +25,13 @@
             }
             ?>
             
-            <div class="row mt">
+            <div class="row data">
               <div class="col-md-12">
                 <div class="content-panel">
-                  <div class="row ms">
-                    <form class="form-inline" role="form">
+                 
+                    <form id="formAbsen" class="form-inline" role="form" action="<?php echo base_url();?>siswa/input_kelas" method="post">
+                    <fieldset id="garisField1">
+                    <fieldset id="garisField2">
                       <div class="form-group">
                         <label class="control-label" for="select">Tahun</label>
                         <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email">
@@ -42,10 +44,9 @@
                         <option>Keterangan</option>
                         <option>No. HP</option>
                       </select>
-                    </form>
 
-                    <br>
-                    <form class="form-inline" role="form" action="<?php echo base_url();?>siswa/input_kelas" method="post">
+                    <br><br>
+                   
                       <div class="form-group">
                         <label class="control-label" for="select">Kelas</label>
                         &nbsp
@@ -63,30 +64,32 @@
                            <?php } ?>
                          </select>
                        </div>
-                     </div>
-                     <button type="submit">daftar</button>
-                     <table class="footable table-bordered table-striped">
+                    <br><br>
+                    <button type="submit" class="btn btn-warning btn-sm"><i class="fa fa-list-alt"></i> Daftar</button>
+                    </fieldset>
+                    </fieldset>
+                    <table class="footable table-bordered table-striped">
 
                       <thead>
                         <tr>
-                          <th><i class="fa fa-bullhorn"></i> NIS</th>
-                          <th class="hidden-phone"><i class="fa fa-question-circle"></i> Nama</th>
-                          <th><i class="fa fa-bookmark"></i> Cek List</th>
+                          <th id="colNomer"> NIS</th>
+                          <th id="colNama" class="hidden-phone"> Nama</th>
+                          <th id="colAksi"> Cek List</th>
                         </tr>
                       </thead>
                       <tbody>
                         <?php foreach ($siswa as $data) {$nis = $data['nis'];?>
                         <tr>
-                          <td><?php echo $data['nis']; ?></td>
-                          <td><?php echo $data['nama'];?></td>
-                          <td><input type="checkbox" name="nis[]" value="<?php echo $data['nis'];
+                          <td id="colNomer"><?php echo $data['nis']; ?></td>
+                          <td id="colNama"><?php echo $data['nama'];?></td>
+                          <td id="colAksi"><input type="checkbox" name="nis[]" value="<?php echo $data['nis'];
                            ?>"></td>
                          </tr>
                          <?php } ?>
                        </tbody>
                        <tfoot>
                         <tr>
-                          <td colspan="5">
+                          <td colspan="3">
                             <div class="pagination pagination-centered hide-if-no-paging"></div>
                           </td>
                         </tr>
