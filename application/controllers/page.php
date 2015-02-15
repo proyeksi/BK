8023d6f247a697034642fdb391f1ef070d5a3637
askmeadi <adi.satria.k@mail.ugm.ac.id>
2015-02-15 15:04:16 +0700

    final

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -1,13 +1,19 @@
 <?php 
 
-	class Page extends CI_Controller{
-		 
-	public function index()
+class Page extends CI_Controller{
+
+	public function index2()
     {
-        $this->load->view('template/header');
-        $this->load->view('template/panel');
-        $this->load->view('home');
-        $this->load->view('template/footer');
+        $akun = $this->session->userdata('akun');
+        if ($akun['login'] == FALSE) {
+            redirect(base_url().'page/index');
+        }
+        else {
+            $this->load->view('template/header');
+            $this->load->view('template/panel');
+            $this->load->view('home');
+            $this->load->view('template/footer');
+        }
     }
 
     public function input()
@@ -21,12 +27,11 @@
     public function rekap()
     {
         $breadcrumb = array(
-            "Beranda" => "index",
-            "Data" => "data",         
-            "Rekap Data" => ""
-        );
+                "Beranda" => "index",
+                "Data" => "rekap",
+                "Rekap Data" => ""
+                );
         $data['breadcrumb'] = $breadcrumb;
-
         $this->load->view('template/header');
         $this->load->view('template/panel');
         $this->load->view('rekap', $data);
@@ -35,32 +40,126 @@
 
     public function edit()
     {
-        $breadcrumb = array(
-            "Beranda" => "index",
-            "Data" => "data",         
-            "Edit Data" => ""
-        );
-        $data['breadcrumb'] = $breadcrumb;
-
-        $this->load->view('template/header');
-        $this->load->view('template/panel');
-        $this->load->view('edit', $data);
-        $this->load->view('template/footer');
+        $akun = $this->session->userdata('akun');
+        if ($akun['login'] == FALSE) {
+            redirect(base_url().'page/index');
+        }
+        else {
+            $breadcrumb = array(
+                "Beranda" => "index",
+                "Data" => "edit",
+                "Input Data Siswa" => ""
+                );
+            $data['breadcrumb'] = $breadcrumb;
+            $this->load->model('siswa_model', 'siswa');
+            $data['siswa'] = $this->siswa->tampilSiswa();
+            $this->load->model('kelas_model','kelas');
+            $data['kelas'] = $this->kelas->tampil();
+            $data['semester'] = $this->kelas->tampilSemester();
+            $this->load->view('template/header');
+            $this->load->view('template/panel');
+            $this->load->view('edit', $data);
+            $this->load->view('template/footer');
+        }
     }
 
     public function data()
     {
-        $breadcrumb = array(
-            "Beranda" => "index",
-            "Data" => "data",         
-            "Data Siswa" => ""
-        );
-        $data['breadcrumb'] = $breadcrumb;
+        $akun = $this->session->userdata('akun');
+        if ($akun['login'] == FALSE) {
+            redirect(base_url().'page/index');
+        }
+        else {
+            $this->load->model('siswa_model', 'siswa');
+            $data['data'] = $this->siswa->tampil();
+            $breadcrumb = array(
+                "Beranda" => "index",
+                "Data" => "data",         
+                "Data Siswa" => ""
+                );
+            $data['breadcrumb'] = $breadcrumb;
+            $this->load->view('template/header');
+            $this->load->view('template/panel');
+            $this->load->view('data', $data);
+            $this->load->view('template/footer');
+        }
+    }
 
-        $this->load->view('template/header');
-        $this->load->view('template/panel');
-        $this->load->view('data', $data);
-        $this->load->view('template/footer');
+    //aziz edit
+    public function index()
+    {
+        $akun = $this->session->userdata('akun');
+        if ($akun['login'] == TRUE) {
+            redirect(base_url().'page/index2');
+        }
+        else{
+            $this->load->view('login');
+        }
+    }
+
+    public function absen()
+    {
+        $akun = $this->session->userdata('akun');
+        if ($akun['login'] == FALSE) {
+            redirect(base_url().'page/index');
+        }
+        else {
+            $this->load->model('siswa_model', 'siswa');
+            $data['data'] = $this->siswa->tampil();
+            $breadcrumb = array(
+                "Beranda" => "index",
+                 "Data" => "absen",
+                "Absensi" => "absen"
+                );
+            $this->load->view('template/header');
+            $this->load->view('template/panel');
+            $this->load->view('absen',$data);
+            $this->load->view('template/footer');
+        }
+    }
+
+    public function semester()
+    {
+        $akun = $this->session->userdata('akun');
+        if ($akun['login'] == FALSE) {
+            redirect(base_url().'page/index');
+        }
+        else {
+            $breadcrumb = array(
+                "Beranda" => "index",
+                "Data" => "semester",         
+                "Tambah Semester" => ""
+                );
+            $data['breadcrumb'] = $breadcrumb;
+            $this->load->model('kelas_model','kelas');
+            $data['semester'] = $this->kelas->tampilSemester();
+            $this->load->view('template/header');
+            $this->load->view('template/panel');
+            $this->load->view('tambah_semester',$data);
+            $this->load->view('template/footer');
+        }
+    }
+
+    public function import()
+    {
+        $akun = $this->session->userdata('akun');
+        if ($akun['login'] == FALSE) {
+            redirect(base_url().'page/index');
+        }
+        else {
+            $this->load->model('kelas_model', 'kelas');
+            $data['data'] = $this->kelas->tampil();
+            $breadcrumb = array(
+                "Beranda" => "index",
+                "Data" => "import",
+                "Import Data Siswa" => ""
+                );
+            $data['breadcrumb'] = $breadcrumb;
+            $this->load->view('template/header');
+            $this->load->view('template/panel');
+            $this->load->view('import',$data);
+            $this->load->view('template/footer');
+        }
     }
 }
 
